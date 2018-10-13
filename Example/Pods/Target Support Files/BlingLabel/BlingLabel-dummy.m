1ae8cbff146a28a8e87596e27c2d9c631f53dcbe
RickWang <woshiwwy16@126.com>
2018-10-13 14:09:36 +0800

    commit test

---
+++ Example/Pods/Target Support Files/BlingLabel/BlingLabel-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_BlingLabel : NSObject
+@end
+@implementation PodsDummy_BlingLabel
+@end
---
+++ Example/Pods/Target Support Files/Pods-BlingLabel_Example/Pods-BlingLabel_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BlingLabel_Example : NSObject
+@end
+@implementation PodsDummy_Pods_BlingLabel_Example
+@end
